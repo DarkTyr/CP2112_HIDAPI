b779f04f4b742bad1f2b2f25d3c51e779853017a
DarkTyr <johnathon.gard@colorado.edu>
2014-09-27 22:46:40 -0600

    The autosend feature wont be used at the moment for the write_read function. It does not
    provided the right answer as per the previous commit. With that said, polling the CP2112
    and then doing a force read once the transfer was successful worked great and reliable.

--- testing/console/CP2112_Testing_Console/main.cpp
+++ testing/console/CP2112_Testing_Console/main.cpp
@@ -346,7 +346,7 @@ int main(int argc, char *argv[])
     memset((void*) &buffer[0], 0x00, sizeof(buffer));
     Sleep(50);
     /// Read What was Read
-    printf("***Read what was read before request3***\n");
+    printf("***Read what was read before request***\n");
     status = hid_read_timeout(device, buffer, 64, 1000);
     if(status == -1)
     {
@@ -380,7 +380,7 @@ int main(int argc, char *argv[])
 
     /// Send Transfer Status request
     printf("***Send Transfer status request***\n");
-    buffer[0] = 0x12;   // Transfer Status request
+    buffer[0] = 0x15;   // Transfer Status request
     buffer[1] = 0x01;   //Request SMBus Transfer Status
     status = hid_write(device, buffer, 2);
     if(status == -1)
@@ -416,6 +416,138 @@ int main(int argc, char *argv[])
         printf(" Buffer[9]: %02hX\n", buffer[7]);
         printf("Buffer[10]: %02hX\n", buffer[10]);
     }
+
+
+    ///Config the CP2112 SMBus for operation
+    printf("***Configure SMBus for operation***\n");
+    printf("***  Autosend will be disabled  ***\n");
+    buffer[0] = 0x06;   // Get/Set SMBus Config
+    buffer[1] = 0x00;   // Clock Speed (100Khz)
+    buffer[2] = 0x01;
+    buffer[3] = 0x86;
+    buffer[4] = 0xA0;
+    buffer[5] = 0x02;   // Device Address
+    buffer[6] = 0x00;   // Auto send read disabled
+    buffer[7] = 0x03;   // Write Time Out of 1000ms
+    buffer[8] = 0xE8;
+    buffer[9] = 0x03;   // Read Time out of 1000ms
+    buffer[10] = 0xE8;
+    buffer[11] = 0x00;   // SCL low timeout disabled
+    buffer[12] = 0x03;   // 2 Retires
+    buffer[13] = 0xE8;
+    status = hid_send_feature_report(device, buffer, 14);
+    if(status == -1)
+    {
+        printf("failed to send SMBus config feature report\n");
+        //string = hid_error(device);
+    }
+    else
+    {
+        printf("SMBus config sent to CP2112\n");
+        printf("Bytes sent: %d \n",status - 1);
+    }
+
+    printf("waiting for 1 second\n");
+    //Sleep(1000);
+
+    memset((void*) &buffer[0], 0x00, sizeof(buffer));
+    /// Send Data Write Read Request
+    printf("***Send Data Read request***\n");
+    buffer[0] = 0x11;   // Data Write Read Request
+    buffer[1] = 0xA4;   // MAXIM IC Fan Controller Address
+    buffer[2] = 0x00;   // Number of bytes to read back
+    buffer[3] = 0x08;
+    buffer[4] = 0x01;   // Number of bytes to send to target
+    buffer[5] = 0x9D;   // Data to send
+    status = hid_write(device, buffer, 6);
+    if(status == -1)
+    {
+        printf("failed to Data Write Read Request\n");
+        //string = hid_error(device);
+    }
+    else
+    {
+        printf("succeded in Data Write Read Request\n");
+        printf("Bytes sent: %d \n",status - 1);
+    }
+
+    memset((void*) &buffer[0], 0x00, sizeof(buffer));
+    Sleep(1000);
+
+    /// Send Transfer Status request
+    printf("***Send Transfer status request***\n");
+    buffer[0] = 0x15;   // Transfer Status request
+    buffer[1] = 0x01;   //Request SMBus Transfer Status
+    status = hid_write(device, buffer, 2);
+    if(status == -1)
+    {
+        printf("failed to send transfer status request \n");
+    }
+    else
+    {
+        printf("Sent Transfer status request\n");
+        printf("Bytes sent: %d \n",status - 1);
+    }
+
+    /// Read Transfer Status Report about the SMBus data writen
+    printf("***Read Transfer status request***\n");
+    status = hid_read_timeout(device, buffer, 64, 1000);
+    if(status == -1)
+    {
+        printf("failed to read status request\n");
+    }
+    else
+    {
+        printf("got status\n");
+        printf("Bytes recieved: %d \n",status - 1);
+        printf(" Buffer[0]: %02hX\n", buffer[0]);
+        printf(" Buffer[1]: %02hX\n", buffer[1]);
+        printf(" Buffer[2]: %02hX\n", buffer[2]);
+        printf(" Buffer[3]: %02hX\n", buffer[3]);
+        printf(" Buffer[4]: %02hX\n", buffer[4]);
+        printf(" Buffer[5]: %02hX\n", buffer[5]);
+        printf(" Buffer[6]: %02hX\n", buffer[6]);
+        printf(" Buffer[7]: %02hX\n", buffer[7]);
+        printf(" Buffer[8]: %02hX\n", buffer[8]);
+        printf(" Buffer[9]: %02hX\n", buffer[7]);
+        printf("Buffer[10]: %02hX\n", buffer[10]);
+    }
+
+    buffer[0] = 0x12;   //Data Read Force Send
+    buffer[1] = 0xFF;   //Number of bytes to send
+
+    status = hid_write(device, buffer, 2);
+
+    status = hid_read(device, buffer, 64);
+    if(status == -1)
+    {
+        printf("failed to read status request\n");
+    }
+    else
+    {
+        printf("got status\n");
+        printf("Bytes recieved: %d \n",status - 1);
+        printf(" Buffer[0]: %02hX\n", buffer[0]);
+        printf(" Buffer[1]: %02hX\n", buffer[1]);
+        printf(" Buffer[2]: %02hX\n", buffer[2]);
+        printf(" Buffer[3]: %02hX\n", buffer[3]);
+        printf(" Buffer[4]: %02hX\n", buffer[4]);
+        printf(" Buffer[5]: %02hX\n", buffer[5]);
+        printf(" Buffer[6]: %02hX\n", buffer[6]);
+        printf(" Buffer[7]: %02hX\n", buffer[7]);
+        printf(" Buffer[8]: %02hX\n", buffer[8]);
+        printf(" Buffer[9]: %02hX\n", buffer[7]);
+        printf("Buffer[10]: %02hX\n", buffer[10]);
+        printf("Buffer[11]: %02hX\n", buffer[11]);
+        printf("Buffer[12]: %02hX\n", buffer[12]);
+        printf("Buffer[13]: %02hX\n", buffer[13]);
+    }
+
+
+
+
+
+
     hid_close(device);
  //3130313031303130h
 
