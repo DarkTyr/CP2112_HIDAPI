7df1e5a4e4a1292e7901fbe58ba0380d862bcc5b
Axel Heider <axelheider@gmx.de>
2021-04-25 22:55:39 +0200

    rename to conf_smbus_attemps
    Rename conf_smbus_retry_time to conf_smbus_attemps
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- python/cp2112_hidapi.py
+++ python/cp2112_hidapi.py
@@ -7,7 +7,7 @@ class CP2112_HIDAPI:
         self.pid = 0xEA90  # Stock CP2112 PID
         self.serial = None
         self._device = hid.device()
-        
+
         self.hidstatus = 0x00
         self.i2cstatus = ''
         '''
@@ -26,7 +26,7 @@ class CP2112_HIDAPI:
         self.conf_smbus_write_timeout = 1000  # 1 second timeout
         self.conf_smbus_read_timeout = 1000  # 1 second timeout
         self.conf_smbus_scl_low_timeout = 0  # disabled
-        self.conf_smbus_retry_time = 0  # 0 retries
+        self.conf_smbus_attempts = 1  # 1 attempt (0 would mean no limit)
 
         self._reportID = {
             'RESET_DEVICE'          : 0x01,
@@ -35,7 +35,7 @@ class CP2112_HIDAPI:
             'SET_GPIO'              : 0x04,
             'GET_VER_INFO'          : 0x05,
             'GETSETSMBUSCONFIG'     : 0x06,
-            
+
             'DATA_READ'             : 0x10,
             'DATA_WRITE_READ'       : 0x11,
             'DATA_READ_FORCE'       : 0x12,
@@ -44,21 +44,21 @@ class CP2112_HIDAPI:
             'XFER_STATUS_REQ'       : 0x15,
             'XFER_STATUS_RESPONSE'  : 0x16,
             'CANCEL_TRANSFER'       : 0x17,
-            
+
             'USB_LOCK'              : 0x20,
             'USB_USB_CONFIG'        : 0x21,
             'USB_MANU_STRING'       : 0x22,
             'USB_PRODUCT_STRING'    : 0x23,
             'USB_SERIAL_STRING'     : 0x24
             }
-        
+
         self._smbusStatusGeneral = {
             0x00: 'BUS_IDLE',
             0x01: 'BUS_BUSY',
             0x02: 'BUS_GOOD',
             0x03: 'BUS_ERROR'
             }
-        
+
         self._smbusStatusBusy = {
             0x00: 'I2C_ADDR_ACK',
             0x01: 'I2C_ADDR_NACK',
@@ -66,7 +66,7 @@ class CP2112_HIDAPI:
             0x03: 'I2C_WR_INPROGRESS',
             0x05: 'I2C_SUCCESS'
             }
-        
+
         self._smbusStatusError = {
             0x00: 'I2C_TIMEOUT_NACK',
             0x01: 'I2C_TIMEOUT_NF',
@@ -87,18 +87,18 @@ class CP2112_HIDAPI:
                 None
 
         return 'could not match VID'
-        
+
     def open_device(self):
         self._device.open(self.vid, self.pid)
         if(self._device.error()):
             return self._device.error()
         else:
             return 'Device Opened'
-        
+
     def configure_gpio(self):
         if(self.device_check() != True):
             return 'No Device Open'
-        
+
         buffer = []
         buffer.append(self._reportID['GETSETGPIOCONFIG'])
         buffer.append(0xFF)
@@ -108,11 +108,11 @@ class CP2112_HIDAPI:
         buffer.append(0x00)
         self.hidstatus = self._device.send_feature_report(buffer)
         return self.hidstatus
-    
+
     def configure_smbus(self):
         if(not self.device_check()):
             return 'No Device Open'
-        
+
         buffer = []
         buffer.append(self._reportID['GETSETSMBUSCONFIG'])
         # buffer.append(0x00)
@@ -139,16 +139,16 @@ class CP2112_HIDAPI:
         buffer.append((self.conf_smbus_read_timeout >>  8) & 0xFF)
         buffer.append((self.conf_smbus_read_timeout >> 00) & 0xFF)
         buffer.append((self.conf_smbus_scl_low_timeout >> 00) & 0xFF)
-        buffer.append((self.conf_smbus_retry_time >>  8) & 0xFF)
-        buffer.append((self.conf_smbus_retry_time >> 00) & 0xFF)
+        buffer.append((self.conf_smbus_attempts >>  8) & 0xFF)
+        buffer.append((self.conf_smbus_attempts >> 00) & 0xFF)
 
         self.hidstatus = self._device.send_feature_report(buffer)
         return self.hidstatus
-    
+
     def get_gpio(self):
         if(not self.device_check()):
             return 'No Device Open'
-        
+
         buffer = []
         buffer = self._device.get_feature_report(self._reportID['GET_GPIO'], 0x02)
         if(buffer[0] == self._reportID['GET_GPIO']):
@@ -161,7 +161,7 @@ class CP2112_HIDAPI:
 
         if(not self.device_check()):
             return 'No Device Open'
-        
+
         buffer = []
         buffer.append(self._reportID['SET_GPIO'])
         buffer.append(gpio)
@@ -171,11 +171,11 @@ class CP2112_HIDAPI:
             return 'Success'
         else:
             return 'Failed to write to Device'
-    
+
     def smbus_write(self, i2cAddress, bytesToSend, data):
         if(not self.device_check()):
             return 'No Device Open', [0x00]
-        
+
         buffer = []
         buffer.append(self._reportID['DATA_WRITE'])
         buffer.append(i2cAddress)
@@ -186,7 +186,7 @@ class CP2112_HIDAPI:
 
         self.hidstatus = self._device.write(buffer)
         self.i2cstatus = 'Status 1: BUSS_BUSY Status 2: I2C_WR_INPROGRESS'
-        
+
         while(self.i2cstatus == 'Status 1: BUSS_BUSY Status 2: I2C_WR_INPROGRESS' or
               self.i2cstatus == 'Status 1: BUS_IDLE Status 2: I2C_WR_INPROGRESS'):
 
@@ -198,26 +198,26 @@ class CP2112_HIDAPI:
                 return 'Unable to write to device', [self.i2cstatus]
             buffer = self._device.read(0x07)
             self._xfer_status_response(buffer)
-            
+
         if(self.i2cstatus == 'Status 1: BUS_GOOD Status 2: I2C_SUCCESS'):
             return 'Success', [self.i2cstatus]
         else:
             return 'Failed', [self.i2cstatus]
-            
+
     def smbus_read(self, i2cAddress, bytesToRead, data):
         if(not self.device_check()):
             return 'No Device Open', [0x00]
-        
+
         buffer = []
         buffer.append(self._reportID['DATA_READ'])
         buffer.append(i2cAddress)
         buffer.append((bytesToRead >> 8) & 0xFF)
         buffer.append((bytesToRead >> 0) & 0xFF)
         self.hidstatus = self._device.write(buffer)
-        
+
         self.i2cstatus = 'Status 1: BUS_BUSY Status 2: I2C_RD_INPROGRESS'
 
-        
+
         while(self.i2cstatus == 'Status 1: BUSS_BUSY Status 2: I2C_WR_INPROGRESS' or
               self.i2cstatus == 'Status 1: BUS_IDLE Status 2: I2C_WR_INPROGRESS'):
             buffer = []
@@ -228,7 +228,7 @@ class CP2112_HIDAPI:
                 return 'Unable to write to device', [0x00]
             buffer = self._device.read(0x07)
             hidBytesRead = self._xfer_status_response(buffer)
-            
+
         status = 'Status 1: BUS_GOOD Status 2: I2C_SUCCESS'
 
         data = []
@@ -239,23 +239,23 @@ class CP2112_HIDAPI:
             buffer.append(0x00)
             buffer.append(0xFF)
             self.hidstatus = self._device.write(buffer)
-            
+
             buffer = self._device.read(0xFF)
             hidBytesRead = self._xfer_status_response(buffer)
             if(buffer[0] == self._reportID['DATA_READ_RESPONSE']):
                 if(buffer[2] > 0x00):
                     for x in range(3, buffer[2] + 3):
-                        data.append(buffer[x]) 
+                        data.append(buffer[x])
                         bytesRead += 1
         if(bytesRead == bytesToRead):
             return 'Success', data
         else:
             return 'Fail', [0x00]
-        
+
     def smbus_write_read(self, i2cAddress, bytesToSend, bytesToRead, data):
         if(not self.device_check()):
             return 'No Device Open', [0x00]
-        
+
         buffer = []
         buffer.append(self._reportID['DATA_WRITE_READ'])
         buffer.append(i2cAddress)
@@ -286,7 +286,7 @@ class CP2112_HIDAPI:
         #if(bytesToRead != hidBytesRead):
         #    return 'Was not able to read all of the Bytes', [self.i2cstatus]
         bytesRead = 0
-        
+
         data = []
         while(bytesRead < bytesToRead):
             buffer = []
@@ -302,21 +302,21 @@ class CP2112_HIDAPI:
                     for x in range(3, buffer[2] + 3):
                         data.append(buffer[x])
                         bytesRead += 1
-                        
+
         if(bytesRead == bytesToRead):
             return 'Success', data
         else:
             return 'Fail', [0x00]
-        
+
     def exit_device(self):
         self._device.close()
-        
+
     def _xfer_status_response(self, data):
         if(data[0] != self._reportID['XFER_STATUS_RESPONSE']):
             return -1
         #  TODO: clean up the BUS_IDLE response and have it make sense
         status1 = self._smbusStatusGeneral[data[1]]
-        
+
         if(status1 == 'BUS_IDLE'):
            status2 = self._smbusStatusBusy[3]
         elif (status1 == 'BUS_BUSY'):
@@ -327,9 +327,9 @@ class CP2112_HIDAPI:
             status2 = self._smbusStatusError[data[2]]
         else:
             return -1
-        
+
         self.i2cstatus = 'Status 1: ' + status1 + ' Status 2: ' + status2
-        
+
         if((status1 == 'BUS_GOOD') and (status2 == 'I2C_SUCCESS')):
             return (data[5] << 8 | data[6])
         else:
